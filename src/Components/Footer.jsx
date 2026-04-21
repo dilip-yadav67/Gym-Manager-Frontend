74546d432d51193fb18f9bc640a759cf610df4db
DILIP YADAV <Dy1574448@gmail.com>
2026-04-22 01:29:20 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,36 +7,40 @@ const Footer = () => {
         <div className="text">
           <h2>About Us</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing <br />
-            elit. Debitis laboriosam quis dolores eveniet quia <br />
-            aliquid aspernatur nam possimus quis
+             Gym Manager simplifies operations. <br />
+             Tracks members & subscriptions. <br />
+             Manages attendance & records. <br />
+             Keeps everything organized.
+
+
+
           </p>
         </div>
 
         <div className="text">
-          <h2>Service</h2>
-          <p>Satisfaction</p>
-          <p>Our Service</p>
-          <p>Price</p>
-          <p>Affordable</p>
-          <p>Quality</p>
+          <h2>Services</h2>
+          <p>Fulfillment</p>
+          <p>Service</p>
+           <p>Value</p>
+          <p>Economy</p>
+           <p>Excellence</p>
         </div>
 
         <div className="text">
           <h2>Training</h2>
-          <p>Trainer</p>
-          <p>Personal</p>
-          <p>Train</p>
-          <p>Offer</p>
-          <p>New</p>
+          <p>Coach</p>
+          <p>Individual</p>
+          <p>Fitness</p>
+          <p>Deals</p>
+          <p>Latest</p>
         </div>
 
         <div className="text">
           <h2>Address</h2>
-          <p>Swift Road</p>
-          <p>Sector-144</p>
-          <p>SR Layout</p>
-          <p>Mumbai</p>
+          <p>Partawal</p>
+          <p>sector-2023</p>
+          <p>D.Y. Layout</p>
+          <p>Maharajganj</p>
         </div>
       </div>
     </>
